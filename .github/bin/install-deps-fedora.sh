6da85c70381b55e4c2b800d3c83d9b60dd5ca146
Matteo Bunino <48362942+matbun@users.noreply.github.com>
2024-09-11 17:52:16 +0200

    Update install-deps-fedora.sh

--- .github/bin/install-deps-fedora.sh
+++ .github/bin/install-deps-fedora.sh
@@ -11,6 +11,11 @@ cmake -DCMAKE_BUILD_TYPE=Release .
 make install || exit 1
 rm -rf cd CGAL-5.6.1
 
+# Install opencascade
+wget https://github.com/Open-Cascade-SAS/OCCT/archive/refs/tags/V7_8_1.tar.gz && tar xzf V7_8_1.tar.gz && rm V7_8_1.tar.gz || exit 1
+cd OCCT-7_8_1 && cmake . && make install || exit 1
+cd .. && rm -rf OCCT-7_8_1 || exit 1
+
 # Install ROOT
 root_version="6.32.04"
 ROOTSYS="/opt/root"
